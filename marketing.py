789bac4d11d5f5dcc4222290887feb87efe05747
isabelly <268587203+evangelistaisabellygois@users.noreply.github.com>
2026-04-12 23:19:24 -0300

    first commit

---
+++ marketing.py
@@ -0,0 +1,17 @@
+print("Olá, deixe aqui sua avaliação para nos ajudar:")
+print("1 - PERFEITO")
+print("2 - SATISFATÓRIO")
+print("1 - RUIM")
+
+Avaliação = int(input("Deixe aqui sua avaliação(1 , 2 ou 3): "))
+#RESULTADOS DAS AVALIAÇÕES
+if Avaliação == 1:
+    resultado = "PERFEITO"
+elif Avaliação == 2:
+    resultado = "SATISFATÓRIO"
+elif Avaliação == 3:    
+    resultado = "RUIM"
+else:  resultado = "AVALIAÇÃO INVÁLIDA"
+#RESULTADO FINAL(agradecimentos)
+print("Sua avaliação foi: ", resultado)
+print("Obrigada pelo feedback!")
